ad2b3e3899e2cd4a04e3d5b0143b190b54bb0ee8
Nevin <nevinsajie@gmail.com>
2024-06-08 23:53:27 +0530

    package to check the login credentials with the database and to save user created accounts to the database

---
+++ models/user.go
@@ -0,0 +1,48 @@
+package models
+
+import (
+	"errors"
+	"hello/models/db"
+	"hello/utils"
+)
+
+type Users struct {
+	Username string
+	Mail     string
+	Password string
+}
+
+func (u *Users) Save() error {
+	query := "INSERT INTO users(username,mail,password) VALUES (?,?,?)"
+	stmt, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+	defer stmt.Close()
+	hashed, erro := utils.HashPassword(u.Password)
+	if erro != nil {
+		return erro
+	}
+	_, err = stmt.Exec(u.Username, u.Mail, hashed)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *Users) Validate() error {
+	query := "SELECT password FROM users WHERE username = ?"
+	row := db.DB.QueryRow(query, u.Username)
+	var retrieved string
+	if err := row.Scan(&retrieved); err != nil {
+		return errors.New("Credentials invalid")
+	}
+
+	passwordValid := utils.Check(u.Password, retrieved)
+
+	if !passwordValid {
+		return errors.New("Credentials invalid")
+	}
+
+	return nil
+}

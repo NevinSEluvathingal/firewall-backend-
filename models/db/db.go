99fd048469a69ded518358f6c30157029c7492cd
Nevin <nevinsajie@gmail.com>
2024-06-20 15:49:12 +0530

    new ip column

--- models/db/db.go
+++ models/db/db.go
@@ -2,52 +2,50 @@ package db
 
 import (
 	"database/sql"
-	"log"
 	"fmt"
-	_"github.com/mattn/go-sqlite3"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 var DB *sql.DB
 
 func InitDB() {
-    var err error
-    DB, err = sql.Open("sqlite3", "api.db") \
-    if err != nil {
-        panic("could not connect to database")
-    }
-    DB.SetMaxOpenConns(10)
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db")
+	if err != nil {
+		panic("could not connect to database")
+	}
+	DB.SetMaxOpenConns(10)
 
-   
-    createTables(DB)
+	createTables(DB)
 }
 
 func createTables(db *sql.DB) {
-    createEventsTable := `
+	createEventsTable := `
     CREATE TABLE IF NOT EXISTS users (
         username TEXT NOT NULL,
         mail TEXT NOT NULL,
         password TEXT NOT NULL,
         usertype TEXT DEFAULT "user",
+        ip TEXT,
         jan INTEGER DEFAULT 0,
         feb INTEGER DEFAULT 0,
-        march INTEGER DEFAULT 0,
-        april INTEGER DEFAULT 0,
+        mar INTEGER DEFAULT 0,
+        apr INTEGER DEFAULT 0,
         may INTEGER DEFAULT 0,
-        june INTEGER DEFAULT 0,
-        july INTEGER DEFAULT 0,
+        jun INTEGER DEFAULT 0,
+        jul INTEGER DEFAULT 0,
         aug INTEGER DEFAULT 0,
-        sept INTEGER DEFAULT 0,
+        sep INTEGER DEFAULT 0,
         oct INTEGER DEFAULT 0,
         nov INTEGER DEFAULT 0,
         dec INTEGER DEFAULT 0
     )`
-    _, err := db.Exec(createEventsTable)
-    if err != nil {
-        log.Fatalf("Error creating users table: %v", err)
-    } else {
-        fmt.Println("Users table created successfully!")
-    }
+	_, err := db.Exec(createEventsTable)
+	if err != nil {
+		log.Fatalf("Error creating users table: %v", err)
+	} else {
+		fmt.Println("Users table created successfully!")
+	}
 }
-
-
-
